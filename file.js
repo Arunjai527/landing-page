965c7444739f57fa1199585814168552955f6cf4
Arunjai527 <109673288+Arunjai527@users.noreply.github.com>
2024-05-17 23:47:43 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,5 @@
+document.getElementById('cta-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    alert(`Thank you for signing up with ${email}!`);
+});
